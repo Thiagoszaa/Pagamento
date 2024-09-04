17da48d8e6577850061db4386ee342f277f3bdaf
Thiago Souza <124935078+Thiagoszaa@users.noreply.github.com>
2024-09-04 16:14:13 -0300

    Update Pagamento.java

--- modelos/Pagamento.java
+++ modelos/Pagamento.java
@@ -2,16 +2,11 @@ package modelos;
 
 public class Pagamento {
 
-    // atributos
     private String nomeFuncionario = "";
     private int matriculaFuncionario = 0;
     private int numeroDependentes = 0;
     private float salarioBase = 0;
-    private float valorGratificao = 0;
-    private float salarioBruto = 0;
-    private float salarioliquido = 0;
-    private float valorDescontoInss = 0;
-    private float valorDescontoIrpf = 0;
+    private float producaoItem = 0;
 
     public String getNomeFuncionario() {
         return nomeFuncionario;
@@ -28,24 +23,6 @@ public class Pagamento {
     public void setMatriculaFuncionario(int matriculaFuncionario) throws Exception {
         if (matriculaFuncionario < 0) throw new Exception("Número de matrícula negativa");
         this.matriculaFuncionario = matriculaFuncionario;
-    }
-
-    public float getValorDescontoInss() {
-        return valorDescontoInss;
-    }
-
-    public void setValorDescontoInss(float valorDescontoInss) throws Exception {
-        if (valorDescontoInss < 0) throw new Exception("Valor INSS negativo");
-        this.valorDescontoInss = valorDescontoInss;
-    }
-
-    public float getValorDescontoIrpf() {
-        return valorDescontoIrpf;
-    }
-
-    public void setValorDescontoIrpf(float valorDescontoIrpf) throws Exception {
-        if (valorDescontoIrpf < 0) throw new Exception("Valor IRPF negativo");
-        this.valorDescontoIrpf = valorDescontoIrpf;
     }
 
     public int getNumeroDependentes() {
@@ -66,85 +43,50 @@ public class Pagamento {
         this.salarioBase = salarioBase;
     }
 
-    public float getValorGratificao() {
-        return valorGratificao;
+    public float getProducaoItem() {
+        return producaoItem;
     }
 
-    public void setValorGratificao(float valorGratificao) throws Exception {
-        if (valorGratificao < 0) throw new Exception("Valor da gratificação negativo");
-        this.valorGratificao = valorGratificao;
+    public void setProducaoItem(float producaoItem) throws Exception {
+        if (producaoItem < 0) throw new Exception("Valor da produção negativo");
+        this.producaoItem = producaoItem;
     }
-
-    public float getSalarioBruto() {
-        return salarioBruto;
-    }
-
-    public void setSalarioBruto(float salarioBruto) {
-        this.salarioBruto = salarioBruto;
-    }
-
-    public float getSalarioliquido() {
-        return salarioliquido;
-    }
-
-    public void setSalarioliquido(float salarioliquido) {
-        this.salarioliquido = salarioliquido;
-    }
-
-    // Métodos
-
 
     public float calcularGratificacao() {
-        if (valorGratificao <= 1000) {
-            valorGratificao = 500.0f;
-        }
-        if (valorGratificao >= 1001 && valorGratificao <= 2000) {
-            valorGratificao = 1250.0f;
-        }
-        if (valorGratificao > 2000) {
-            valorGratificao = 2250.0f;
-        }
-        return valorGratificao;
+        if (producaoItem <= 1000) return 500.0f;
+        if (producaoItem <= 2000) return 1250.0f;
+        return 2250.0f;
     }
 
     public float calcularSalarioBruto() {
-        return salarioBase + valorGratificao;
-    }
-
-    public float calcularDescontoFilhos() {
-        return numeroDependentes * 123.0f;
+        return salarioBase + calcularGratificacao();
     }
 
     public float calcularDescontoInss() {
-        if (salarioBruto <= 1412.00f) {
-            valorDescontoInss = salarioBruto * 0.075f;
-        } else if (salarioBruto <= 2666.68f) {
-            valorDescontoInss = salarioBruto * 0.09f;
-        } else if (salarioBruto <= 4000.03f) {
-            valorDescontoInss = salarioBruto * 0.12f;
-        } else {
-            valorDescontoInss = salarioBruto * 0.14f;
-        }
-        return valorDescontoInss;
+        float salarioBruto = calcularSalarioBruto();
+
+        if (salarioBruto <= 1412.00f) return salarioBruto * 0.075f;
+        if (salarioBruto <= 2666.68f) return salarioBruto * 0.09f;
+        if (salarioBruto <= 4000.03f) return salarioBruto * 0.12f;
+        return salarioBruto * 0.14f;
     }
 
     public float calcularDescontoIrpf() {
-        float salarioBruto = getSalarioBruto();
-        float descontoIrpf = 0;
+        float salarioBruto = calcularSalarioBruto();
 
-        if (salarioBruto <= 2259.20f) {
-            descontoIrpf = 0; // Isento
-        } else if (salarioBruto >= 2259.21f && salarioBruto <= 2826.65f) {
-            descontoIrpf = salarioBruto * 0.075f; // 7,5%
-        } else if (salarioBruto >= 2826.66f && salarioBruto <= 3751.05f) {
-            descontoIrpf = salarioBruto * 0.15f; // 15%
-        } else if (salarioBruto >= 3751.06f && salarioBruto <= 4664.68f) {
-            descontoIrpf = salarioBruto * 0.225f; // 22,5%
-        } else {
-            descontoIrpf = salarioBruto * 0.275f; // 27,5%
-        }
+        if (salarioBruto < 2259.20f) return 0;
+        if (salarioBruto <= 2826.65f) return salarioBruto * 0.075f;
+        if (salarioBruto <= 3751.05f) return salarioBruto * 0.15f;
+        if (salarioBruto <= 4664.68f) return salarioBruto * 0.225f;
+        return salarioBruto * 0.275f;
+    }
 
-        return descontoIrpf;
+    public float calcularDescontoDependentes() {
+        return numeroDependentes * 123;
+    }
+
+    public float calcularSalarioLiquido() {
+        return calcularSalarioBruto() - calcularDescontoInss() - calcularDescontoIrpf() + calcularDescontoDependentes();
     }
 
 }
